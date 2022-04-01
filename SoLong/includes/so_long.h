c4e5057a018ebc53dfcbb6e488a50324be8c1487
Diogo Silverio pereira <dsilveri@c1r12s1.42lisboa.com>
2022-04-01 16:29:00 +0100

    added functions to init assets and map

--- SoLong/includes/so_long.h
+++ SoLong/includes/so_long.h
@@ -10,7 +10,14 @@
 # define COLLECTIBLE	'C'
 # define EXIT			'E'
 # define PLAYER			'P'
+# define GRASS			'0'
+# define TREE			'1'
 
+# define GRASS_PATH 	"./assets/grass.xpm"
+# define TREE_PATH 		"./assets/tree.xpm"
+# define PLAYER_PATH 	"./assets/player.xpm"
+
+# define IMG_SIZE		60
 
 typedef struct s_map
 {
@@ -18,6 +25,14 @@ typedef struct s_map
 	int height;
 	char **map;
 } t_map;
+
+typedef struct s_imgs
+{
+	void *grass;
+	void *tree;
+	void *player;
+}	t_imgs; 
+
 
 /* get_map.c */
 t_map get_map(char *map_path);
--- SoLong/srcs/so_long.c
+++ SoLong/srcs/so_long.c
@@ -1,20 +1,5 @@
 #include "so_long.h"
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
 
 // função de teste durante o desenvolvimento
 void print_map(t_map map)
@@ -34,48 +19,58 @@ void print_map(t_map map)
 	}
 }
 
+t_imgs init_assets(void *mlx)
+{
+	t_imgs imgs;
+	int img_width;
+	int img_height;
+
+	imgs.grass = mlx_xpm_file_to_image(mlx, GRASS_PATH, &img_width, &img_height);
+	printf("GRASS: size: %i x %i\n", img_width, img_height);
+	printf("poiter to img: |%p|\n", imgs.grass);
+	imgs.tree = mlx_xpm_file_to_image(mlx, TREE_PATH, &img_width, &img_height);
+	printf("TREE: size: %i x %i\n", img_width, img_height);
+	imgs.player = mlx_xpm_file_to_image(mlx, PLAYER_PATH, &img_width, &img_height);
+	printf("TREE: size: %i x %i\n", img_width, img_height);
+	return (imgs);
+}
+
+
+void init_map(t_map map, t_imgs imgs, void	*mlx, void	*mlx_win)
+{
+	int i;
+	int j;
+
+	printf("poiter to img: |%p|\n", imgs.grass);
+	i = 0;
+	while (i < map.height)
+	{
+		j = 0;
+		while (j < map.length)
+		{
+			if (map.map[i][j] == GRASS)
+				mlx_put_image_to_window(mlx, mlx_win, imgs.grass, j * IMG_SIZE, i * IMG_SIZE);
+			else if (map.map[i][j] == TREE)
+				mlx_put_image_to_window(mlx, mlx_win, imgs.tree, j * IMG_SIZE, i * IMG_SIZE);
+			else if (map.map[i][j] == PLAYER)
+			{
+				mlx_put_image_to_window(mlx, mlx_win, imgs.grass, j * IMG_SIZE, i * IMG_SIZE);
+				mlx_put_image_to_window(mlx, mlx_win, imgs.player, j * IMG_SIZE, i * IMG_SIZE);
+			}	
+			printf("%c", map.map[i][j]);
+			j++;
+		}
+		i++;
+		printf("\n");
+	}
+}
+
 int main(int argc, char **argv)
 {
+	t_map 	map;
+	t_imgs	imgs;
 	void	*mlx;
 	void	*mlx_win;
-	void	*img;
-	void	*img1;
-	void	*img2;
-	void	*img3;
-	char	*relative_path = "./assets/grass1.xpm";
-	char	*relative_path1 = "./assets/multiple_three.xpm";
-	char	*relative_path2 = "./assets/multiple_three1.xpm";
-	char	*relative_path3 = "./assets/multiple_three2.xpm";
-	int		img_width;
-	int		img_height;
-	t_map 	map;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 500, 500, "SoLong");
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	img1 = mlx_xpm_file_to_image(mlx, relative_path1, &img_width, &img_height);
-	img2 = mlx_xpm_file_to_image(mlx, relative_path2, &img_width, &img_height);
-	img3 = mlx_xpm_file_to_image(mlx, relative_path3, &img_width, &img_height);
-
-	printf("imagem size: %i x %i\n", img_width, img_height);
-
-	
-	mlx_put_image_to_window(mlx, mlx_win, img, 60, 60);
-	
-	mlx_put_image_to_window(mlx, mlx_win, img3, 0, 0);
-	mlx_put_image_to_window(mlx, mlx_win, img3, 0, 60);
-	mlx_put_image_to_window(mlx, mlx_win, img3, 0, 120);
-	mlx_put_image_to_window(mlx, mlx_win, img3, 0, 180);
-
-	mlx_put_image_to_window(mlx, mlx_win, img1, 0, 440);
-	mlx_put_image_to_window(mlx, mlx_win, img1, 60, 440);
-	mlx_put_image_to_window(mlx, mlx_win, img1, 120, 440);
-	mlx_put_image_to_window(mlx, mlx_win, img1, 180, 440);
-
-	mlx_put_image_to_window(mlx, mlx_win, img2, 0, 0);
-	mlx_put_image_to_window(mlx, mlx_win, img2, 60, 0);
-	mlx_put_image_to_window(mlx, mlx_win, img2, 120, 0);
-	mlx_put_image_to_window(mlx, mlx_win, img2, 180, 0);
 
 	if (argc != 2)
 	{
@@ -83,8 +78,10 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	map = get_map(argv[1]);
-	print_map(map);
-
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, map.length * IMG_SIZE, map.height * IMG_SIZE, "SoLong");	
+	imgs = init_assets(mlx);
+	init_map(map, imgs, mlx, mlx_win);
 	mlx_loop(mlx);
 	return (0);
 }
